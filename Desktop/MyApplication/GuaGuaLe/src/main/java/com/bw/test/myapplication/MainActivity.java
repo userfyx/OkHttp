6f051c929f29be282bdeecfa87bcc62fd83f7af3
user_fanyongxing <2450907310@qq.com>
2016-11-01 20:58:16 +0800

    Dome

--- Desktop/MyApplication/GuaGuaLe/src/main/java/com/bw/test/myapplication/MainActivity.java
+++ Desktop/MyApplication/GuaGuaLe/src/main/java/com/bw/test/myapplication/MainActivity.java
@@ -1,25 +1,38 @@
 package com.bw.test.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button enterbtn;
+    private Button view;
+    private Button touch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        enterbtn = (Button) findViewById(R.id.enterbtn);
-//        enterbtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(MainActivity.this,"出现了",Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        view = (Button)findViewById(R.id.view_click);
+
+        touch = (Button)findViewById(R.id.touch_click);
+        view.setOnClickListener(this);
+        touch.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.view_click:
+                Intent intent=new Intent(MainActivity.this,ViewActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.touch_click:
+                Intent intent1=new Intent(MainActivity.this,TouchActivity.class);
+                startActivity(intent1);
+                break;
+        }
     }
 }
